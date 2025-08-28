0f65e69ae6f7a56ada8d1c82c03cf4996b3fd931
datadiego <juandiegomariscal@gmail.com>
2025-08-28 08:32:16 +0200

    update

--- modules/bootloader.nix
+++ modules/bootloader.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  boot.loader.grub.enable = false;
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.grub.device = "/dev/vda";
+  boot.loader.grub.enable = true;
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.grub.device = "/dev/nvme0n1";
   boot.loader.grub.useOSProber = true;
   boot.loader.systemd-boot.configurationLimit = 3;
 }
