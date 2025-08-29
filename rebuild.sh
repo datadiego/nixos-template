060063ee3fe60b85e12776c98dd1954f49b32e58
datadiego <juandiegomariscal@gmail.com>
2025-08-30 00:37:14 +0200

    new generation

--- rebuild.sh
+++ rebuild.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
 check1=$(git diff ./configuration.nix /etc/nixos/configuration.nix)
 check2=$(git diff ./home.nix ~/.config/home-manager/home.nix)
 
 # Comprobar las diferencias en configuration.nix
 if [ -n "$check1" ]; then
   echo -e "🚨 El archivo ./configuration.nix tiene diferencias con /etc/nixos/configuration.nix"
+  git diff ./configuration.nix /etc/nixos/configuration.nix
 else
   echo -e "✅ El archivo /etc/nixos/configuration.nix está actualizado respecto a ./configuration.nix"
 fi
@@ -12,13 +12,23 @@ fi
 # Comprobar las diferencias en home.nix
 if [ -n "$check2" ]; then
   echo -e "🚨 El archivo ./home.nix tiene diferencias con ~/.config/home-manager/home.nix"
+  git diff ./home.nix ~/.config/home-manager/home.nix
 else
   echo -e "✅ El archivo ~/.config/home-manager/home.nix está actualizado respecto a ./home.nix"
 fi
 
-sudo cp -r * /etc/nixos/
-sudo nixos-rebuild switch
+sudo cp configuration.nix /etc/nixos/configuration.nix
+sudo cp -r /modules /etc/nixos/
+sudo cp -r /users /etc/nixos/
 
-cp -r * .config/home-manager/
-home-manager switch
+cp home.nix ~/.config/home-manager/home.nix
+cp -r dotfiles ~/.config/home-manager/
+cp -r scripts ~/.config/home-manager/
+
+sudo nixos-rebuild switch
+sudo home-manager switch
+
+git add .
+git commit -m "new generation"
+
 
