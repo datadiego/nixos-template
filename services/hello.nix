2a9c3bd9bcf44c9f01827165a73950cd51f61a2d
datadiego <juandiegomariscal@gmail.com>
2025-08-30 23:34:56 +0200

    new gen

--- services/hello.nix
+++ services/hello.nix
@@ -5,7 +5,8 @@
     description = "Test de servicios";
     wantedBy = [ "multi-user.target" ];
     serviceConfig = {
-      ExecStart = "${pkgs.bash}/bin/bash -c 'date >> /tmp/date'";
+      ExecStart = "${pkgs.bash}/bin/bash" ;
+      ExecStartArgs = [ "-c" "date >> /tmp/date" ];
       Restart = "always";
       User = "root";
     };
