b02bc9acf8d5747e90bd5c43d4aaf26afb14d216
datadiego <juandiegomariscal@gmail.com>
2025-08-30 23:51:11 +0200

    new gen

--- services/hello.nix
+++ services/hello.nix
@@ -1,25 +1,14 @@
-{ config, lib, pkgs, ... }:
+{ config, pkgs, ... }:
 
-let
-  myService = {
+{
+  systemd.services.hello-service = {
+    description = "My Hello NixOS Service";
+    after = [ "network.target" ];
+    wantedBy = [ "multi-user.target" ];
+
     serviceConfig = {
-      ExecStart = "${pkgs.bash}/bin/bash /etc/nixos/scripts/hello.sh";
+      ExecStart = "/etc/nixos/scripts/hello.sh";
+      Restart = "always";
     };
   };
-in
-{
-  systemd.services.myCustomService = {
-    description = "My Custom Service";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = myService.serviceConfig;
-    after = [ "network.target" ];
-    path = [ pkgs.bash ];
-    # script removed to avoid ExecStart conflict
-  };
-
-  environment.systemPackages = [ pkgs.bash ];
-
-  system.activationScripts.myCustomScript = ''
-    echo "Hello from my custom service!" > /tmp/hello
-  '';
 }
