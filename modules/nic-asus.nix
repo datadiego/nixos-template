3d612a7b35f96fdc3f5b837d521bfecd7a5b3f4b
datadiego <juandiegomariscal@gmail.com>
2025-08-30 23:04:13 +0200

    new gen

--- modules/nic-asus.nix
+++ modules/nic-asus.nix
@@ -3,15 +3,20 @@
 
   systemd.network.links = {
     "10-custom-eth0" = {
-      matchConfig.MACAddress = "d4:5d:64:62:c3:9b";
-      linkConfig.Name = "eth0";
+      matchConfig.MACAddress = "d4:5d:64:62:c3:9b";  # MAC original para hacer match
+      linkConfig = {
+        Name = "eth0";
+        MACAddress = "DE:AD:13:37:BE:EF";  # Nueva MAC
+      };
     };
 
     "10-custom-wlan0" = {
       matchConfig.MACAddress = "70:66:55:cf:27:8f";
-      linkConfig.Name = "wlan0";
+      linkConfig = {
+        Name = "wlan0";
+        MACAddress = "E7:CA:FE:13:37:0";
+      };
     };
   };
 
-  
 }
