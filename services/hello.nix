4fc8c6874d036426ad6d13ae7af307bb041423b0
datadiego <juandiegomariscal@gmail.com>
2025-08-30 23:54:31 +0200

    new gen

--- services/hello.nix
+++ services/hello.nix
@@ -2,13 +2,20 @@
 
 {
   systemd.services.hello-service = {
-    description = "My Hello NixOS Service";
-    after = [ "network.target" ];
-    wantedBy = [ "multi-user.target" ];
-
+    description = "Run hello.sh periodically";
     serviceConfig = {
       ExecStart = "/etc/nixos/scripts/hello.sh";
       Restart = "always";
+    };
+  };
+
+  systemd.timers.hello-service = {
+    description = "Timer for hello-service";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "5min";         # Espera 5 minutos después de arrancar
+      OnUnitActiveSec = "10min"; # Se ejecuta cada 10 minutos
+      Unit = "hello-service.service";
     };
   };
 }
