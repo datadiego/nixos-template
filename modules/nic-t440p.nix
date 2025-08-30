077a6c1c979c9d90362d8034cb1d3660ddfe8eba
datadiego <juandiegomariscal@gmail.com>
2025-08-30 21:51:21 +0200

    new gen

--- modules/nic-t440p.nix
+++ modules/nic-t440p.nix
@@ -3,16 +3,20 @@
 
   systemd.network.links = {
     "10-custom-eth0" = {
-      matchConfig.MACAddress = "28:d2:44:32:da:28";
-      linkConfig.Name = "eth0";
+      matchConfig.MACAddress = "28:d2:44:32:da:28";  # MAC original para hacer match
+      linkConfig = {
+        Name = "eth0";
+        MACAddress = "DE:AD:DE:AD:DE:AD";  # Nueva MAC
+      };
     };
 
     "10-custom-wlan0" = {
       matchConfig.MACAddress = "5c:51:4f:88:7d:c2";
-      linkConfig.Name = "wlan0";
+      linkConfig = {
+        Name = "wlan0";
+        MACAddress = "0F:F1:C1:A7:13:37";
+      };
     };
   };
 
-  networking.interfaces.eth0.macAddress = "DE:AD:DE:AD:DE:AD";
-  networking.interfaces.wlan0.macAddress ="0F:F1:C1:A7:13:37";
 }
