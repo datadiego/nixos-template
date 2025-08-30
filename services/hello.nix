3253b1990dd27c37b19eb6d02cb5e2542e0161ef
datadiego <juandiegomariscal@gmail.com>
2025-08-30 23:36:49 +0200

    new gen

--- services/hello.nix
+++ services/hello.nix
@@ -4,11 +4,12 @@
   systemd.services.hello = {
     description = "Test de servicios";
     wantedBy = [ "multi-user.target" ];
+    # ...existing code...
     serviceConfig = {
-      ExecStart = "${pkgs.bash}/bin/bash" ;
-      ExecStartArgs = [ "-c" "date >> /tmp/date" ];
+      ExecStart = "${pkgs.bash}/bin/bash -c \"date >> /tmp/date\"";
       Restart = "always";
       User = "root";
     };
+# ...existing code...
   };
 }
