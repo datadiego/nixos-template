0be9baa41bc62c87d9c2d126113dd3c0aba8788e
datadiego <juandiegomariscal@gmail.com>
2025-08-30 09:50:45 +0200

    new gen

--- rebuild.sh
+++ rebuild.sh
@@ -1,14 +1,22 @@
 NIXOS_PATH="/etc/nixos/"
 HOME_PATH="$HOME/.config/home-manager/"
 
-sudo cp -r ./modules $NIXOS_PATH
-sudo cp -r ./users $NIXOS_PATH
-cp -r ./modules $HOME_PATH
-cp -r ./dotfiles $HOME_PATH
-cp -r ./scripts $HOME_PATH
+CONFIG_FILE="./configuration.nix"
 
-sudo cp ./configuration.nix $NIXOS_PATH
-cp ./home.nix $HOME_PATH
+if [[ "$1" == "--thinkpad440" ]]; then
+  CONFIG_FILE="./configuration-t440p.nix"
+fi
+
+sudo cp -r ./modules "$NIXOS_PATH"
+sudo cp -r ./users "$NIXOS_PATH"
+
+cp -r ./modules "$HOME_PATH"
+cp -r ./dotfiles "$HOME_PATH"
+cp -r ./scripts "$HOME_PATH"
+
+sudo cp "$CONFIG_FILE" "$NIXOS_PATH"
+
+cp ./home.nix "$HOME_PATH"
 
 sudo nixos-rebuild switch
 home-manager switch
