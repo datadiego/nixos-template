362d9084f92a971b03c1f7c34e01ccc5e8f9dd7f
datadiego <juandiegomariscal@gmail.com>
2025-08-30 09:12:20 +0200

    new gen

--- modules/bootloader.nix
+++ modules/bootloader.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  boot.loader.grub.enable = true;
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.grub.device = "/dev/nvme0n1";
+  boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
   boot.loader.systemd-boot.configurationLimit = 3;
 }
