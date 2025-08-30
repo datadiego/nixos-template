580548da72dc6340c257771e587c15c32a837d5b
datadiego <juandiegomariscal@gmail.com>
2025-08-30 21:33:55 +0200

    new gen

---
+++ modules/nic-asus.nix
@@ -0,0 +1,15 @@
+{
+  networking.usePredictableInterfaceNames = false;
+
+  systemd.network.links = {
+    "10-custom-eth0" = {
+      matchConfig.MACAddress = "d4:5d:64:62:c3:9b";
+      linkConfig.Name = "eth0";
+    };
+
+    "10-custom-wlan0" = {
+      matchConfig.MACAddress = "70:66:55:cf:27:8f";
+      linkConfig.Name = "wlan0";
+    };
+  };
+}
