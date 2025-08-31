db0d60c7aa470afde932a570258ac0b2072013de
datadiego <juandiegomariscal@gmail.com>
2025-08-31 08:45:27 +0200

    new gen

--- modules/nic-asus.nix
+++ modules/nic-asus.nix
@@ -6,7 +6,7 @@
       matchConfig.MACAddress = "d4:5d:64:62:c3:9b";  # MAC original para hacer match
       linkConfig = {
         Name = "eth0";
-        MACAddress = "DE:AD:13:37:BE:EF";  # Nueva MAC
+        # MACAddress = "DE:AD:13:37:BE:EF";
       };
     };
 
@@ -14,9 +14,7 @@
       matchConfig.MACAddress = "70:66:55:cf:27:8f";
       linkConfig = {
         Name = "wlan0";
-        MACAddress = "ba:dc:af:e1:33:70";
-
-
+        # MACAddress = "ba:dc:af:e1:33:70";
       };
     };
   };
