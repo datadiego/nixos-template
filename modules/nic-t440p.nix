be541ea0f36ba638ad1cf50b4755ddcace37e364
datadiego <juandiegomariscal@gmail.com>
2025-08-31 09:13:16 +0200

    new gen

--- modules/nic-t440p.nix
+++ modules/nic-t440p.nix
@@ -6,7 +6,7 @@
       matchConfig.MACAddress = "28:d2:44:32:da:28";  # MAC original para hacer match
       linkConfig = {
         Name = "eth0";
-        MACAddress = "DE:AD:DE:AD:DE:AD";  # Nueva MAC
+        # MACAddress = "DE:AD:DE:AD:DE:AD";
       };
     };
 
@@ -14,7 +14,7 @@
       matchConfig.MACAddress = "5c:51:4f:88:7d:c2";
       linkConfig = {
         Name = "wlan0";
-        MACAddress = "0F:F1:C1:A7:13:37";
+        # MACAddress = "0F:F1:C1:A7:13:37";
       };
     };
   };
