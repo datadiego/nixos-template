e2e34f54101d8bf428ca0a910f40730deb47e4bd
datadiego <juandiegomariscal@gmail.com>
2025-09-01 21:29:28 +0200

    new gen

--- configuration.nix
+++ configuration.nix
@@ -14,13 +14,13 @@
     ./modules/nic-asus.nix
     ./modules/virtualization.nix
     ];
-    
-  security.wrappers.ubridge = {
-    source = "${pkgs.ubridge}/bin/ubridge";
-    owner = "root";
-    group = "root";
-    permissions = "4755";
-  };
+
+  # security.wrappers.ubridge = {
+  #   source = "${pkgs.ubridge}/bin/ubridge";
+  #   owner = "root";
+  #   group = "root";
+  #   permissions = "4755";
+  # };
   services.upower.enable = true;
   nixpkgs.config.allowUnfree=true;
   services.printing.enable=true;
