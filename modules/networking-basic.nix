9a76f529823be67d98aaaeaaea17f64d09e16f7f
datadiego <juandiegomariscal@gmail.com>
2025-09-04 10:33:46 +0200

    new gen

--- modules/networking-basic.nix
+++ modules/networking-basic.nix
@@ -3,5 +3,13 @@
 {
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  # networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+
+  networking.networkmanager.settings = ''
+    [main]
+    dns=none
+
+    [global-dns-domain-*]
+    servers=1.1.1.1;8.8.8.8;
+  '';
 }
